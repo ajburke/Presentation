2ff5b8b31099e80b55754c5b5124937ca4acda12
ajburke <alexander.burke@taxware.com>
2014-01-20 12:15:06 -0500

    created divisibleByThree and divisibleByFive functions

--- Problem1.rb
+++ Problem1.rb
@@ -11,12 +11,66 @@ class Problem1
 		end
 		total
 	end
+
+	def divisibleByThree (number)
+		if number % 3 == 0
+			true
+		else
+			false
+		end
+	end
+
+	def divisibleByFive(number)
+		if number % 5 == 0
+			true
+		else
+			false
+		end
+	end
 end
 
 
 
 require 'test/unit'
 class Problem1_Test < Test::Unit::TestCase
+
+	def test_true_divisibleByThree
+		cut = Problem1.new
+
+		actualAnswer = cut.divisibleByThree(9)
+		expectedAnswer = true
+
+		assert_equal actualAnswer, expectedAnswer
+
+	end
+
+	def test_true_divisibleByFive
+		cut = Problem1.new
+
+		actualAnswer = cut.divisibleByFive(15)
+		expectedAnswer = true
+
+		assert_equal actualAnswer, expectedAnswer
+	end
+
+	def test_false_divisibleByThree
+		cut = Problem1.new
+
+		actualAnswer = cut.divisibleByThree(10)
+		expectedAnswer = false
+
+		assert_equal actualAnswer, expectedAnswer
+
+	end
+
+	def test_false_divisibleByFive
+		cut = Problem1.new
+
+		actualAnswer = cut.divisibleByFive(14)
+		expectedAnswer = false
+
+		assert_equal actualAnswer, expectedAnswer
+	end	
 
 	def test_findSum
 		cut = Problem1.new
